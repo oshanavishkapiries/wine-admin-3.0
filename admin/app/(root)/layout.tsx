76fb2089615163ec1190af2e3a0b9618298d4f04
oshanavishka6 <oshanavishka6@gmail.com>
2025-01-03 05:18:09 +0000

    sider-layout-done

---
+++ admin/app/(root)/layout.tsx
@@ -0,0 +1,23 @@
+import { AppSidebar } from '@/components/common/app-sidebar';
+import { SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar';
+
+const layout = ({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) => {
+  return (
+    <SidebarProvider>
+      <AppSidebar />
+      <main className="w-full min-h-screen bg-gray-100 flex flex-col">
+        <div className="w-fill h-[50px] flex flex-row p-4 items-center justify-between">
+          <SidebarTrigger />
+        </div>
+
+        {children}
+      </main>
+    </SidebarProvider>
+  );
+};
+
+export default layout;
